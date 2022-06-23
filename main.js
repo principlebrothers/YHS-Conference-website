d6e4fa6da3a157143c9a5aa9b7cfb94aee4b442d
principlebrothers <principlesbrothers@yahoo.com>
2022-06-23 01:12:50 +0000

    Embed video from youtube in the about section

--- main.js
+++ main.js
@@ -13,44 +13,44 @@ link.forEach((link) => link.addEventListener('click', () => {
 const speakers = [
   {
     position: 1,
-    name: "Apostle Eric Nyamekye",
-    personality: "Chairman of the Church of Pentecost",
-    image: "./img/Apostle-Eric.jpg",
-    alt: "Apostle Eric Nyamekye",
-    description: "He has worked with the youth for 20 decade. He holds a doctrate degree in theology."
+    name: 'Apostle Eric Nyamekye',
+    personality: 'Chairman of the Church of Pentecost',
+    image: './img/Apostle-Eric.jpg',
+    alt: 'Apostle Eric Nyamekye',
+    description: 'He has worked with the youth for 20 decade. He holds a doctrate degree in theology.',
   },
   {
     position: 2,
-    name: "Elder Prince Adjei",
-    personality: "Elder of the Church of Pentecost",
-    image: "./img/Elder-Prince.png",
-    alt: "Elder Prince Adjei",
-    description: "He has great knowledge in family life and as he is an internationally aclaimed speaker"
+    name: 'Elder Prince Adjei',
+    personality: 'Elder of the Church of Pentecost',
+    image: './img/Elder-Prince.png',
+    alt: 'Elder Prince Adjei',
+    description: 'He has great knowledge in family life and as he is an internationally aclaimed speaker',
   },
   {
     position: 3,
-    name: "Prophet Justice Lemawu",
-    personality: "Doctor in Divinity",
-    image: "./img/Prophet-Justice.png",
-    alt: "Prophet Justice Lemawu",
-    description: "He is the senior Presbyter of Christ Cosmopolitan, a church he founded whiles studying at the KTU, where he rose to fame."
+    name: 'Prophet Justice Lemawu',
+    personality: 'Doctor in Divinity',
+    image: './img/Prophet-Justice.png',
+    alt: 'Prophet Justice Lemawu',
+    description: 'He is the senior Presbyter of Christ Cosmopolitan, a church he founded whiles studying at the KTU, where he rose to fame.',
   },
   {
     position: 4,
-    name: "Ernest Anyewe Adonu",
-    personality: "Author of some of best-selling books and a Counsellor",
-    image: "./img/Ernest.png",
-    alt: "Ernest Anyewe Adonu",
-    description: "He he an internationally acclaimed speaker, author, Counsellor and Life coach."
+    name: 'Ernest Anyewe Adonu',
+    personality: 'Author of some of best-selling books and a Counsellor',
+    image: './img/Ernest.png',
+    alt: 'Ernest Anyewe Adonu',
+    description: 'He he an internationally acclaimed speaker, author, Counsellor and Life coach.',
   },
 ];
 
 const speakersContainer = document.querySelector('.speakers-grid-container');
 
 function createSpeakers() {
-  let len = speakers.length;
+  const len = speakers.length;
   for (let i = 0; i < len; i += 1) {
-    let speaker = speakers[i];
+    const speaker = speakers[i];
 
     speakersContainer.innerHTML += `
       <div class="speakers-card">
